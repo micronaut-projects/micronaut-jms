61721ba9cca82d0b212d8d15b0a6e9a4990d3bad
Elliott Pope <epope@bu.edu>
2022-11-13 21:54:53 -0500

    Adding Topic, Queue, and Broker classes Broker is the abstract object from which Queue and Topic are derived holding common information (destination, transactional, acknowledgement, etc.) Broker forces its subclasses to be able to generate themselves from their respective annotations Topic adds support for shared and durable which are not available for Queue Topic and Queue objects will be registered with a MessageListener to avoid bloating the register methods on the ListenerContainer

--- jms-core/src/main/java/io/micronaut/jms/listener/Queue.java
+++ jms-core/src/main/java/io/micronaut/jms/listener/Queue.java
@@ -1,2 +1,46 @@
-package io.micronaut.jms.listener;public class Queue {
+package io.micronaut.jms.listener;
+
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.core.type.Argument;
+import io.micronaut.inject.ExecutableMethod;
+import io.micronaut.messaging.annotation.MessageBody;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Queue extends Broker implements javax.jms.Queue {
+    private Queue(String destination, int acknowledgeMode, boolean transacted, String messageSelector, Class<?> targetClass) {
+        super(destination, acknowledgeMode, transacted, messageSelector, targetClass);
+    }
+
+    public static Queue fromAnnotation(
+            ExecutableMethod<?, ?> method,
+            AnnotationValue<io.micronaut.jms.annotations.Queue> annotation) {
+        final Class<?> targetClass = Stream.of(method.getArguments())
+                .filter(arg ->
+                        arg.isDeclaredAnnotationPresent(MessageBody.class) ||
+                                arg.isDeclaredAnnotationPresent(io.micronaut.jms.annotations.Message.class))
+                .findAny()
+                .map(Argument::getClass)
+                .get();
+
+        final String destination = annotation.getRequiredValue(String.class);
+        final int acknowledgeMode = annotation.getRequiredValue("acknowledgeMode", Integer.class);
+        final boolean transacted = annotation.getRequiredValue("transacted", Boolean.class);
+        final Optional<String> messageSelector = annotation.get("messageSelector", String.class);
+
+        return new Queue(destination, acknowledgeMode, transacted, messageSelector.orElse(null), targetClass);
+    }
+
+    @Override
+    public String getQueueName() throws JMSException {
+        return this.getName();
+    }
+
+    public Destination getDestination(Session session) throws JMSException {
+        return session.createQueue(this.name);
+    }
 }
--- jms-core/src/main/java/io/micronaut/jms/listener/Topic.java
+++ jms-core/src/main/java/io/micronaut/jms/listener/Topic.java
@@ -1,4 +1,62 @@
 package io.micronaut.jms.listener;
 
-public class Topic {
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.core.type.Argument;
+import io.micronaut.inject.ExecutableMethod;
+import io.micronaut.messaging.annotation.MessageBody;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Topic extends Broker implements javax.jms.Topic {
+    private final boolean shared;
+
+    private final boolean durable;
+
+    public Topic(String destination, int acknowledgeMode, boolean transacted, String messageSelector, Class<?> targetClass, boolean shared, boolean durable) {
+        super(destination, acknowledgeMode, transacted, messageSelector, targetClass);
+        this.shared = shared;
+        this.durable = durable;
+    }
+
+    public static Topic fromAnnotation(
+            ExecutableMethod<?, ?> method,
+            AnnotationValue<io.micronaut.jms.annotations.Topic> annotation) {
+        final Class<?> targetClass = Stream.of(method.getArguments())
+                .filter(arg ->
+                        arg.isDeclaredAnnotationPresent(MessageBody.class) ||
+                                arg.isDeclaredAnnotationPresent(io.micronaut.jms.annotations.Message.class))
+                .findAny()
+                .map(Argument::getClass)
+                .get();
+
+        final String destination = annotation.getRequiredValue(String.class);
+        final int acknowledgeMode = annotation.getRequiredValue("acknowledgeMode", Integer.class);
+        final boolean transacted = annotation.getRequiredValue("transacted", Boolean.class);
+        final Optional<String> messageSelector = annotation.get("messageSelector", String.class);
+        final boolean shared = annotation.getRequiredValue("shared", Boolean.class);
+        final boolean durable = annotation.getRequiredValue("durable", Boolean.class);
+
+        return new Topic(destination, acknowledgeMode, transacted, messageSelector.orElse(null), targetClass, shared, durable);
+    }
+
+    @Override
+    public String getTopicName() throws JMSException {
+        return this.getName();
+    }
+
+    public boolean isShared() {
+        return shared;
+    }
+
+    public boolean isDurable() {
+        return durable;
+    }
+
+    public Destination getDestination(Session session) throws JMSException {
+        return session.createTopic(this.name);
+    }
 }
