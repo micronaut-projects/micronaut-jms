184ceb8c88be1f3b7b431807699898fa0cb05ed7
Elliott Pope <epope@bu.edu>
2022-11-13 21:58:34 -0500

    Migrating to a simpler Listener/ListenerFactory paradigm instead of the bloated JMSListenerContainer classes Listener registers a Broker type (either Queue or Topic) and a MessageListener, it is able to determine what type of MessageConsumer to generate (shared, shared-durable, durable, or plain) and inject MessageSelectors etc. (eventually it can also handle injecting ExceptionHandlers) Listener will hold references to Connection, Session, MessageConsumer, and Broker and will release the Session/Connection when shutdown is called (either programatically or via Micronaut's IoC Context) ListenerFactory simply registers Listeners either as beans or as directly referenced objects (directly referenced will have to be a later iteration of the API when more dynamic control of Queues/Topics is required)

--- jms-core/src/main/java/io/micronaut/jms/listener/ListenerFactory.java
+++ jms-core/src/main/java/io/micronaut/jms/listener/ListenerFactory.java
@@ -1,4 +1,23 @@
 package io.micronaut.jms.listener;
 
+import io.micronaut.context.BeanContext;
+import io.micronaut.inject.qualifiers.Qualifiers;
+import io.micronaut.jms.pool.JMSConnectionPool;
+import jakarta.inject.Singleton;
+
+import javax.jms.MessageListener;
+
+@Singleton
 public class ListenerFactory {
+    private final BeanContext context;
+
+    public ListenerFactory(BeanContext context) {
+        this.context = context;
+    }
+
+    public <T extends Broker> void register(String connectionFactory, T queue, MessageListener listener) {
+        final JMSConnectionPool connectionPool = context.getBean(
+                JMSConnectionPool.class, Qualifiers.byName(connectionFactory));
+        context.registerSingleton(Listener.class, Listener.register(connectionPool, queue, listener), Qualifiers.byName(queue.getName()));
+    }
 }
