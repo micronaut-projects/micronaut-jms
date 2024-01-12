519c0f00a80e54074310094ae1305a7eb87d8e8b
Sergio del Amo <sergio.delamo@softamo.com>
2024-01-12 13:43:26 +0100

    update to major version

---
+++ jms-sqs/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("io.micronaut.build.internal.jms-module")
+}
+
+dependencies {
+    annotationProcessor(mn.micronaut.graal)
+    annotationProcessor(mnValidation.micronaut.validation.processor)
+    implementation(mnValidation.micronaut.validation)
+    api(projects.micronautJmsCore)
+    api(libs.amazon.sqs.messaging)
+    api(libs.aws.sqs)
+    api(mnAws.micronaut.aws.sdk.v2)
+    compileOnly(libs.graal.svm)
+}
