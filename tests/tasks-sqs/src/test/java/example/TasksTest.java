37ec46e2c9822dd9ffe3108e7b41e15b9913a918
Sergio del Amo <sergio.delamo@softamo.com>
2026-02-24 14:29:00 +0100

    diable in native image localstack test

--- tests/tasks-sqs/src/test/java/example/TasksTest.java
+++ tests/tasks-sqs/src/test/java/example/TasksTest.java
@@ -1,22 +1,21 @@
 package example;
 
-import io.micronaut.context.annotation.Property;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.micronaut.test.support.TestPropertyProvider;
-import jakarta.inject.Named;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 
+@DisabledInNativeImage
 @Testcontainers(disabledWithoutDocker = true)
 @MicronautTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
